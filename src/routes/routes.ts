a333d4d2ba3d00e719481ae2c29a779e7a72d868
Alexandre Junior <35452628+insertjuninho@users.noreply.github.com>
2022-08-28 19:16:56 -0300

    Update routes.ts

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,7 +5,9 @@ import { Router } from "express";
 
 const routes = Router();
 
-
+router.get("/", (req, res) => {
+    res.send('API V1.0');
+})
 routes.post('/register', new userController().handle);
 routes.post('/auth', new userController().auth);
 routes.post('/laudo', new laudoController().handle).use(authMiddleware);
